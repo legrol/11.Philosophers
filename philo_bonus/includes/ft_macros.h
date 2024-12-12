cbacba14f5c503303509c45e8252fcdb20d2d63a
rdel-olm <rdel-olm@student.42malaga.com>
2024-12-12 15:40:34 +0100

    Update colors and macros

--- philo_bonus/includes/ft_macros.h
+++ philo_bonus/includes/ft_macros.h
@@ -19,12 +19,11 @@
 # define EAT				"is eating"
 # define DIED				"died"
 
-# define USAGE_ERR			"❗Arguments: Usage: ./philo <# philosophers> \
-									<time to die> <time to eat> <time to \
-									sleep> [# times each philosopher must \
-									eat]\n"
-# define NUM_ARGV_ERR		"❗1️⃣ Incorrect number of arguments, expected \
-								one."
+# define USAGE_ERR			"❗Arguments: Usage: ./philo <Nbr philosophers> \
+<time to die> <time to eat> <time to sleep> [Nbr times each philosopher must \
+eat]"
+# define NUM_ARGV_ERR		"❗1️⃣ Incorrect number of arguments, expected min 4\
+ o max 5."
 # define NO_INT_ARGV_ERR	"❗Argument is not integer value"
 # define INT_NEG_ARGV_ERR	"❗Argument with negativ integer value. Forbidden."
 # define PARAMS_ERR			"❗Incorrect parameteres."
@@ -33,8 +32,12 @@
 # define NEGATIVE			" negative value."
 # define VALUES_INVALID		"Values of some parameter are invalid."
 # define INIT_ERR			"Initialization error."
+# define THREADS_ERR		"Threads error."
+# define BYE				"Fix the argument, my friend."
 
 # define INT_MAX			2147483647
 # define INT_MIN			-2147483648
+# define UNLOCK				1
+# define LOCK				0
 
 #endif
--- philo_bonus/src/philoshopers_bonus.c
+++ philo_bonus/src/philoshopers_bonus.c
@@ -20,47 +20,52 @@
  * 
  */
 
-static void	ft_check_params(t_envp *envp, int argc, char *argv[])
+static int	ft_check_params(t_envp *envp, int argc, char *argv[])
 {
 	int	i;
 
-	i = 0;
+	i = 1;
 	while (i < argc)
 	{
 		if (!ft_isinteger(argv[i]))
 		{
 			printf(RED ARG "%i" INVALID "\n" RESET, i);
-			ft_manage_err(GREEN NO_INT_ARGV_ERR RESET);
+			return (ft_manage_err_simple(NO_INT_ARGV_ERR), EXIT_FAILURE);
 		}
 		if (ft_philo_atoi(argv[i]) < 0)
 		{
 			printf(RED ARG "%i" NEGATIVE "\n" RESET, i);
-			ft_manage_err(GREEN INT_NEG_ARGV_ERR RESET);
+			return (ft_manage_err_simple(INT_NEG_ARGV_ERR), EXIT_FAILURE);
 		}
 		i++;
 	}
 	ft_init_struct(envp, argc, argv);
-	if (envp->philo_eat_limit < 1 || envp->time_to_die < 1 || \
-		envp->time_to_eat < 1 || envp->time_to_sleep < 1 || \
-		envp->nbr_philos < 1)
+	if (envp->philo_eat_limit < 0 || envp->time_to_die < 0 || envp->\
+	time_to_eat < 0 || envp->time_to_sleep < 0 || envp->nbr_philos < 1)
 	{
 		printf(RED VALUES_INVALID "\n" RESET);
-		ft_manage_err(GREEN PARAMS_ERR RESET);
+		return (ft_manage_err_simple(PARAMS_ERR), EXIT_FAILURE);
 	}
+	return (EXIT_SUCCESS);
 }
 
 int	main(int argc, char *argv[])
 {
 	t_envp	envp;
 
+	ft_print_banner();
 	envp.eat_max = 0;
 	envp.stopping_rule = 0;
-	if (argc < 5 || argv > 6)
-		ft_manage_err(USAGE_ERR);
-	ft_check_params(&envp, argc, argv);
-	if (!ft_init(&envp))
-		ft_manage_err(GREEN INIT_ERR RESET);
-
-
-	return (0);
+	if (argc < 5 || argc > 6)
+	{
+		ft_manage_err_simple(NUM_ARGV_ERR);
+		return (ft_manage_err_simple(USAGE_ERR), EXIT_FAILURE);
+	}
+	if (ft_check_params(&envp, argc, argv))
+		return (ft_manage_err_simple(BYE), EXIT_FAILURE);
+	if (ft_init_sim(&envp))
+		return (ft_manage_err_simple(INIT_ERR), EXIT_FAILURE);
+	if (ft_create_threads_and_monitor(&envp))
+		return (ft_manage_err_simple(THREADS_ERR), EXIT_FAILURE);
+	return (EXIT_SUCCESS);
 }
