a780c160a23340f806c544891eccfdae39323796
rdel-olm <rdel-olm@student.42malaga.com>
2024-12-27 12:33:04 +0100

    Problems datarace

--- philo/errors/ft_manage_err.c
+++ philo/errors/ft_manage_err.c
@@ -6,21 +6,13 @@
 /*   By: rdel-olm <rdel-olm@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/24 22:13:04 by rdel-olm          #+#    #+#             */
-/*   Updated: 2024/12/09 21:13:05 by rdel-olm         ###   ########.fr       */
+/*   Updated: 2024/12/27 12:32:42 by rdel-olm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
 
 /**
- * The function "ft_manage_err" displays an error message in a formatted 
- * manner using colored text and then terminates the program by calling 
- * `exit(EXIT_FAILURE)`.
- * 
- * @param const char *err			A string containing the error message 
- * 									to be displayed.
- * @return void
- * 
  * The function "ft_manage_err_simple" displays an error message in a 
  * formatted manner using colored text. Unlike "ft_manage_err", it does not 
  * terminate the program and is intended for logging or non-critical errors.
@@ -30,12 +22,6 @@
  * @return void
  * 
  */
-
-void	ft_manage_err(const char *err)
-{
-	printf(RED "Error: " RESET GREEN "%s\n" RESET, err);
-	exit (EXIT_FAILURE);
-}
 
 void	ft_manage_err_simple(const char *err)
 {
